30761caeca4af1a9bb9610585dac24afc2f4a251
Dewmith Weerasena <75196537+Dewmith@users.noreply.github.com>
2024-03-09 18:06:48 +0100

    Only Buildings Filtering for .tif files
    Codes for filtering out .tif considering ONLY buildings (Contains land or vegetation but no Solar Panels)

---
+++ FIltering_Only_building_pan.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+
+def list_files(folder_path, extension=".tif"):
+    #List all TIF files in the specified folder.
+    tif_files = [file for file in os.listdir(folder_path) if file.endswith(extension)]
+    return tif_files
+
+def Copy_to_folders_pan(folder_pan,folder_copy_pan):
+    try:
+        # Check if the provided path is a directory
+        if os.path.isdir(folder_pan):
+            # Get tif files of the directory
+            tif_files = list_files(folder_pan)
+            
+            # Print the names of all files
+            for file in text_file:
+                for pan in tif_files:
+                    if pan[:6] == file[:-11]:
+                        shutil.copy(os.path.join(folder_pan, pan), os.path.join(folder_copy_pan, pan))
+        else:
+            print(f"{folder_pan} is not a valid directory.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# For Pan folders
+folder_pan = r'C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\src_pan'
+folder_copy_pan = r'C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\#Sorted_patches_pan_xs\Only Buildings\building_pan'
+
+#For xs folders
+#folder_xs = r'C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\src_xs'
+#folder_copy_xs = r'C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\src_xs_with_buildings_pv'
+
+#Text file with the file names to be moved
+text_f = r'C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\#Sorted_patches_pan_xs\Only Buildings\building.txt'
+text_file = open(text_f, 'r')
+
+
+Copy_to_folders_pan(folder_pan,folder_copy_pan)
+
+
+
+
+
---
+++ FIltering_Only_building_patch.py
@@ -0,0 +1,28 @@
+import rasterio
+import os
+import shutil
+
+def list_files(folder_path, extension=".tif"):
+    #List all TIF files in the specified folder.
+    tif_files = [file for file in os.listdir(folder_path) if file.endswith(extension)]
+    return tif_files
+
+def contain_buildings_pv(tif_files, folder_path, output_txt_file, output_folder): 
+    
+    with open(output_txt_file, 'w') as txt_file:
+         for patch in tif_files:
+             with rasterio.open(os.path.join(folder_path, patch)) as src: # Open the dataset using Rasterio
+                 data = src.read(1) 
+                 if (1 in data) and (2 not in data):
+                     txt_file.write(patch + '\n')
+                     shutil.copy(os.path.join(folder_path, patch), os.path.join(output_folder, patch))
+
+    
+
+folder_path = r"C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\labeled_patches"
+output_txt_file = r"C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\#Sorted_patches_pan_xs\Only Buildings\building.txt"
+output_folder = r"C:\Users\dewmi\OneDrive - Fondation EPF\Uni\4eme Année\Project\Images\data_v2\#Sorted_patches_pan_xs\Only Buildings\building_patches"
+
+tif_files = list_files(folder_path)
+contain_buildings_pv(tif_files, folder_path, output_txt_file, output_folder)
+
